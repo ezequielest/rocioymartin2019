455e63443887ef71676e9485492c6d3ea31ac7ec
Ezequiel <ezequiel@MacBook-Pro-de-Ezequiel.local>
2019-11-10 18:32:00 -0300

    se agregaron imagenes y los retoques de estilo final

--- send-email.php
+++ send-email.php
@@ -6,66 +6,59 @@ require 'mail/src/Exception.php';
 require 'mail/src/PHPMailer.php';
 require 'mail/src/SMTP.php';
 
+$mail = new PHPMailer(true); 
 
-/*if ( !isset($_POST['name']) && $_POST['name'] == '' ||
-     !isset($_POST['email']) && $_POST['email'] == '' ||
-     !isset($_POST['message']) && $_POST['message'] == '' ) 
-{*/
-
-$response = [];
-if ( !isset($_POST['name']) || 
-     !isset($_POST['email']) ||
-     !isset($_POST['message']) ||
-     !isset($_POST['personasConfirmadas']) ) 
-{
-    die();
-    $response['thereIsError'] = true;
+// Passing `true` enables exceptions
+$datos = array();
+if (!isset($_POST['name']) || $_POST['name'] == "" &&
+    !isset($_POST['personasConfirmadas']) || $_POST['personasConfirmadas'] == "" &&
+    !isset($_POST['message']) || $_POST['message'] == ""
+    ) {
+    $datos['mensaje'] = "error";
+    echo json_encode($datos);
 } else {
-    $mail = new PHPMailer(true);                              // Passing `true` enables exceptions
-    try {
-        //Server settings
-        $mail->SMTPDebug = 0;                                 // Enable verbose debug output
-        $mail->isSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com';                   // Specify main and backup SMTP servers
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'ezequiel.estigarribia@gmail.com';               // SMTP username
-        $mail->Password = '4739eerr';                         // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
-        $mail->Port = 587;                                    // TCP port to connect to2
 
-        //Recipients
-        $mail->setFrom('ezequie.estigarribia@gmail.com', 'Invite');
-        $mail->addAddress('invite@blur.com.ar', 'Invite');     // Add a recipient
-        $mail->addAddress('info@blur.com.ar', 'Blur');     // Add a recipient
 
-        //Attachments
-        if (isset($_FILES['attachFile'])) {
-            $mail->AddAttachment($_FILES['attachFile']['tmp_name'],$_FILES['attachFile']['name']); 
-        }
+try {
+    //Server settings
+    $mail->SMTPDebug = 0;                                   // Enable verbose debug output
+    $mail->isSMTP();                                        // Set mailer to use SMTP
+    $mail->Host = 'smtp.gmail.com';                         // Specify main and backup SMTP servers
+    $mail->SMTPAuth = true;                                 // Enable SMTP authentication
+    $mail->Username = 'invite.invitacion@gmail.com';    // SMTP username
+    $mail->Password = '4739eerr';                           // SMTP password
+    $mail->SetFrom('invite.invitacion@gmail.com','Invite');
+    $mail->SMTPSecure = 'tls';                              // Enable TLS encryption, `ssl` also accepted
+    $mail->Port = 587;                                      // TCP port to connect to
 
-        //Content
-        $mail->isHTML(true);                                  // Set email format to HTML
-        $mail->Subject  = 'Contacto desde INVITE';
-        $mail->Body     = "<b></b>Nuevo mensaje enviado desde invite.blur</b> <br><br>
-                          Nombre: " . $_POST['name'] . "<br>
-                          Email: " . $_POST['email'] . "<br>
-                          Cantidad de personas confirmadas: " . $_POST['personasConfirmadas'] . "<br>";
-        
-        if (isset($_POST['phone']) && $_POST['phone'] != '') {
-            $mail->Body .= "Tel&eacute;fono: " . $_POST['phone'] . "<br>";
-        }
+    //Recipients
+    $mail->setFrom('invite.invitacion@gmail.comm', 'Invite');
+    $mail->addAddress('dulcesisabella@hotmail.com', 'Rocio y Martin');     // Add a recipient
 
-        $mail->Body     .="Mensaje: " . $_POST['message'] . "<br>";
+    //Attachments
+    //$mail->AddAttachment($_FILES['attachFile']['tmp_name'],$_FILES['attachFile']['name']); 
 
-        //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        $mail->send();
-        $response['thereIsError'] = false;
-        //echo 'Message has been sent';
-    } catch (Exception $e) {
-        echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
+    //Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = 'Nueva confirmación de invitado';
+    $mail->Body     = 'Felicitaciones, hay una nueva confirmaci&oacute;n para tu boda!<br><br>';
+    $mail->Body    .='<b>Nombre:</b> ' . $_POST['name'] . '<br>';
+    $mail->Body    .= '<b>Cantidad de personas confirmadas:</b> ' . $_POST['personasConfirmadas'].'<br>';
+    
+    if (isset($_POST['phone']) || $_POST['phone'] != ''){
+        $mail->Body    .= '<b>Tel&eacute;fono:</b> ' . $_POST['phone'].'<br>';
     }
+    
+    $mail->Body    .= '<b>Mensaje:</b><br><br>' . $_POST['message'].'<br>';
+    $mail->AltBody = $_POST['nombre'] . ' ir&oacute; a tu fiesta!!</b>';
 
+    $mail->send();
+
+    $datos['mensaje'] = "ok";
+
+    echo json_encode($datos);
+    
+} catch (Exception $e) {
+    echo 'Message could not be sent. Mailer Error: ', $mail->ErrorInfo;
 }
-
-echo json_encode($response);
+}
